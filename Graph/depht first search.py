5a0dd0df897d7edb9b9f9d882d42fc4d278ede66
atikin111 <vronches@yandex.ru>
2025-08-18 21:57:37 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	(({{[[]]}})).py

--- Graph/depht first search.py
+++ Graph/depht first search.py
@@ -1,6 +1,7 @@
 class Node:
     def __init__(self, value):
         self.value = value
+
         self.outbound = []
         self.inbound = []
 
@@ -37,7 +38,60 @@ c.point_to(d)
 d.point_to(a)
 b.point_to(d)
 
-g = Graph(a)
-result = g.dfs()
+g1 = Graph(a)
 
-print(str(node) for node in result)
+print([str(node) for node in g1.dfs()])
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+a.point_to(b)
+b.point_to(c)
+
+g2 = Graph(a)
+
+print([str(node) for node in g2.dfs()])
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+a.point_to(b)
+b.point_to(c)
+c.point_to(d)
+d.point_to(a)
+b.point_to(d)
+b.point_to(f)
+c.point_to(e)
+
+g3 = Graph(a)
+
+print([str(node) for node in g3.dfs()])
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+g = Node('g')
+h = Node('h')
+i = Node('i')
+k = Node('k')
+a.point_to(b)
+b.point_to(c)
+c.point_to(d)
+d.point_to(a)
+b.point_to(d)
+a.point_to(e)
+e.point_to(f)
+e.point_to(g)
+f.point_to(i)
+f.point_to(h)
+g.point_to(k)
+
+g4 = Graph(a)
+
+print([str(node) for node in g4.dfs()])
